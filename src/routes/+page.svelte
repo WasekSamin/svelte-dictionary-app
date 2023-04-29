9a97da06bee8b1f98d12f4be7e89b2160a32dd77
WasekSamin <wasek.samin47@gmail.com>
2023-04-29 15:26:41 +0600

    29th april, 2023

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
     import Navbar from "../components/headers/Navbar.svelte";
     import DictionaryCard from "../components/home/DictionaryCard.svelte";
-    import Footer from "../components/footer/Footer.svelte";
 </script>
 
 <Navbar />
 <DictionaryCard />
-<Footer />
