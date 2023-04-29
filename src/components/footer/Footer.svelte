d6665b90c8830f88946e3a874eaddf726756df1d
WasekSamin <wasek.samin47@gmail.com>
2023-04-29 15:23:50 +0600

    29th april, 2023

---
+++ src/components/footer/Footer.svelte
@@ -0,0 +1,6 @@
+<div class="w-full max-w-screen-2xl py-5 px-5 md:px-10 lg:px-20 bg-stone-800 mt-80">
+    <div class="flex items-center justify-center">
+        <p>Build By <a href="https://github.com/WasekSamin" target="_blank" class="text-sky-400 hover:text-sky-500 hover:underline transition-colors duration-200 ease-linear">Wasek Samin</a>
+        </p>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import Navbar from "../components/headers/Navbar.svelte";
     import DictionaryCard from "../components/home/DictionaryCard.svelte";
+    import Footer from "../components/footer/Footer.svelte";
 </script>
 
 <Navbar />
 <DictionaryCard />
+<Footer />
